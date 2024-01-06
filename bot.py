c556a9f61617a15d70b26c0ead04dd49aa9f8582
surajkmr00 <87617962+surajkmr00@users.noreply.github.com>
2024-01-06 18:07:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def start(bot: Client, cmd: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Updates Channel", url="https://t.me/VJ_Botz")
+                        InlineKeyboardButton("Updates Channel", url="https://t.me/+TI1b15VrvARIMTJI")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
