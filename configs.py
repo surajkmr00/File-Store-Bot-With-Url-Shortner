be8f601ad1d87c59090024353b4239a2211d7410
surajkmr00 <87617962+surajkmr00@users.noreply.github.com>
2024-01-06 18:50:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "instantlinks.in")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "a23c7884b317233e245f6dd89e11bb1c5d1889d9")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "817926999"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://surajkmr0852:fQLn4ngQ6AvYGK21@cluster0.kczshos.mongodb.net/?retryWrites=true&w=majority")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://surajkmr0852:wLUXL4yOQKI7pYUF@cluster0.kczshos.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001514387008")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002139381092"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
